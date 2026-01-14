de31b0dc0b08ab95583c442ac19ebb784b6309da
haohuan <haohuan@synopsys.com>
2026-01-13 18:40:31 -0800

    init repo from submodule with my changes

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,22 @@
+return {
+  'zbirenbaum/copilot.lua',
+  enabled = false,
+  cmd = 'Copilot',
+  build = ':Copilot auth',
+  event = 'InsertEnter',
+  opts = {
+    panel = { enabled = false, layout = { position = 'right', ratio = 0.4 } },
+    suggestion = { enabled = false, auto_trigger = true },
+    filetypes = {
+      c = true,
+      cpp = true,
+      lua = true,
+      python = true,
+      markdown = true,
+      help = true,
+    },
+  },
+  config = function(_, opts)
+    require('copilot').setup(opts)
+  end,
+}
