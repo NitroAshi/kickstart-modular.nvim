fca72a6312ece24b07b60b782472139788bb89e3
haohuan <haohuan@synopsys.com>
2026-01-14 23:16:04 -0800

    chore: update plugin lazy loading and config best practices

--- lua/custom/plugins/blink-cmp-copilot.lua
+++ lua/custom/plugins/blink-cmp-copilot.lua
@@ -1,4 +1,6 @@
+-- Load blink-cmp-copilot lazily on VeryLazy event
 return {
   "giuxtaposition/blink-cmp-copilot",
+  event = "VeryLazy", -- lazy load on VeryLazy event
   dependencies = { "zbirenbaum/copilot.lua", "saghen/blink.cmp" },
 }
